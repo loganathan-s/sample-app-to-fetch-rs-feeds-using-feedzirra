b46aa446a8b554e9390d0b02fc07ff32fcbbb2a6
Logan <loganathan.ms@gmail.com>
2013-04-12 20:49:14 +0530

    Activerecord query updated

--- app/controllers/feed_entries_controller.rb
+++ app/controllers/feed_entries_controller.rb
@@ -3,12 +3,12 @@ class FeedEntriesController < ApplicationController
   
   # Display feeds in chronological order
   def index
-     @feed_entries =  FeedEntry.paginate(:page => params[:page], :per_page => NUMBER_OF_FEEDS_PER_PAGE).order( "published_at DESC")
+     @feed_entries =  FeedEntry.select([:id, :title]).paginate(:page => params[:page], :per_page => NUMBER_OF_FEEDS_PER_PAGE).order( "published_at DESC")
   end
 
 # Retrieve feed content
   def show
-    @feed_entry = FeedEntry.where(id: params[:id]).first
+    @feed_entry = FeedEntry.select([:id, :content]).where(id: params[:id]).first
   end
   
   private
