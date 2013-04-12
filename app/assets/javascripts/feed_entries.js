7ff0ab64a0a186b6e6154472d3a4f3401f967a34
Logan <loganathan.ms@gmail.com>
2013-04-13 04:50:12 +0530

    Jquery added to avoid duplicate ajax call

--- app/assets/javascripts/feed_entries.js
+++ app/assets/javascripts/feed_entries.js
@@ -1,4 +1,16 @@
-$(function() {$(".content").hide();});
+$(function() {
+  $(".content").hide();
+  $(".feed_link").click(function(){
+   // Check feed content is fetched already or not
+   var content = !$.trim( $(this).next(".content").html() ).length;
+
+   if(!content) {
+      $(this).next(".content").slideToggle("10");
+      // Disable ajax call if content is fetched already
+      return false;
+    }
+  });
+});
 	
 
 
